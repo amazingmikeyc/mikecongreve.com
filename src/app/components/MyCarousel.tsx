77495f0769f28ebd4503709593870fbf52def5a2
Mike Congreve <mike.congreve@gmail.com>
2026-04-27 20:58:40 +0100

    Update MyCarousel to fix type definitions, adjust props cast, and enable unoptimized images in config

--- src/app/components/MyCarousel.tsx
+++ src/app/components/MyCarousel.tsx
@@ -1,13 +1,13 @@
 'use client'
 import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import {ReactNode} from "react";
+import React from "react";
 
-
-export default function MyCarousel({ children }: { children: ReactNode })
+export default function MyCarousel({ children }: { children: React.ReactElement[] })
 {
     return (
-    <Carousel  autoFocus={true} showIndicators={true} showThumbs={false} showStatus={false} infiniteLoop={true} showArrows={true}>
-        {children}
-    </Carousel>)
+        <Carousel autoFocus={true} showIndicators={true} showThumbs={false} showStatus={false} infiniteLoop={true} showArrows={true}>
+            {children as any}
+        </Carousel>
+    );
 }
